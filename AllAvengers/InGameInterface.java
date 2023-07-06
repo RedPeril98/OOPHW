5b91e279702c819ee28f0decf461d372e8027230
Дарья <red_peril@bk.ru>
2023-07-06 18:39:44 +0300

    Initial commit

---
+++ AllAvengers/InGameInterface.java
@@ -0,0 +1,9 @@
+package AllAvengers;
+
+import java.util.ArrayList;
+
+public interface InGameInterface {
+
+    void step(ArrayList<Avengers> units);
+    String getInfo();
+}
---
+++ Names.java
@@ -0,0 +1,11 @@
+public enum Names {
+
+    IronMan("Tony Stark"), Thor("Thor Odinson"), Hulk("Bruce Banner"), Wasp("Janet Van Dyne"),
+    AntMan("Dr.Henry Jonathan Pym"), CaptainAmerica("Steven Rogers"), BlackWidow("Natasha Romanoff"), SpiderMan("Peter Parker");
+
+    private String name;
+
+    Names(String name) {this.name = name;}
+
+    public String getName() {return name;}
+}
